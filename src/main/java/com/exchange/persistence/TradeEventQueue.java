ecee641346b3b7f53cb8cd758bb079c24781d6ac
Lhloworld <bmbboommaxboom@gmail.com>
2026-02-25 14:48:38 +0530

    Made the TradeEventQueue and Matching Engine

---
+++ src/main/java/com/exchange/persistence/TradeEventQueue.java
@@ -0,0 +1,18 @@
+package com.exchange.persistence;
+
+import com.exchange.model.Trade;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class TradeEventQueue {
+
+    private final BlockingQueue<Trade> queue=new LinkedBlockingQueue<>();
+
+    public void publish(Trade t){
+        queue.offer(t);
+    }
+
+    public Trade take() throws InterruptedException{
+        return queue.take();
+    }
+}
