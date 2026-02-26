91035dfdb10b1f0d8d16458ca928c3f8664c2e67
Lhloworld <bmbboommaxboom@gmail.com>
2026-02-26 10:59:24 +0530

    Added the Test to check for concurrency

---
+++ src/test/MatchingEngineConcurrencyTest.java
@@ -0,0 +1,35 @@
+package com.exchange;
+
+import com.exchange.engine.*;
+import com.exchange.model.*;
+import com.exchange.orderbook.*;
+import com.exchange.persistence.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.*;
+
+public class MatchingEngineConcurrencyTest {
+
+    @Test
+    void stressTest() throws Exception {
+
+        OrderBook book=new OrderBook();
+        TradeEventQueue q=new TradeEventQueue();
+        MatchingEngine engine=new MatchingEngine(book,q);
+
+        ExecutorService es=Executors.newFixedThreadPool(50);
+
+        for(int i=0;i<1000;i++){
+            long id=i;
+            es.submit(() -> {
+                Order o=new Order(id,id,
+                        id%2==0?OrderSide.BUY:OrderSide.SELL,
+                        100,10);
+                engine.process(o);
+            });
+        }
+
+        es.shutdown();
+        es.awaitTermination(10,TimeUnit.SECONDS);
+    }
+}
